e3c0300c69d483cf7491c1b666c13c632e8fd7c2
Michal Siron Mac <michal.siron@centrum.cz>
2018-08-12 11:58:32 +0200

    Finished 4/11 v trainingu

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,27 @@
-var gulp = require('gulp');
+let gulp = require('gulp');
+let watch = require('gulp-watch');
 
 
 gulp.task('default', function () {
     console.log("Hello, this is my first gulp task");
+});
+
+gulp.task('html', function () {
+    console.log("This is not default gulp task but html.");
+});
+
+gulp.task('styles', function () {
+    console.log("Here we can run som Sass of another plugins.");
+});
+
+gulp.task('watch', function () {
+
+    watch("./app/index.html", function () {
+        gulp.start('html');
+    });
+
+    watch("./app/assets/styles/**/*.css", function () {
+        gulp.start('styles');
+    });
+
 });
