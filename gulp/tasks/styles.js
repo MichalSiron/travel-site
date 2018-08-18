731e82ac0afec451f6a07b681a3b07ba80cf915d
Michal Siron Mac <michal.siron@centrum.cz>
2018-08-18 08:36:22 +0200

    konec 6/18

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -8,5 +8,9 @@ let cssImport = require('postcss-import');
 gulp.task('styles', function () {
     return gulp.src('./app/assets/styles/styles.css')
         .pipe(postcss([cssImport, autoprefixer, cssvars, nestedcss]))
+        .on('error', function (errorInfo) {
+            console.log(errorInfo.toString());
+            this.emit('end');
+        })
         .pipe(gulp.dest('./app/temp/styles'));
 });
