3b45acf8c6be4f25a5a11b87734936de88f49642
Michal Siron Mac <michal.siron@centrum.cz>
2018-08-19 09:08:27 +0200

    konec 7/19

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -4,10 +4,11 @@ let autoprefixer = require('autoprefixer');
 let cssvars = require('postcss-simple-vars');
 let nestedcss = require('postcss-nested');
 let cssImport = require('postcss-import');
+let mixins = require('postcss-mixins');
 
 gulp.task('styles', function () {
     return gulp.src('./app/assets/styles/styles.css')
-        .pipe(postcss([cssImport, autoprefixer, cssvars, nestedcss]))
+        .pipe(postcss([cssImport, mixins, autoprefixer, cssvars, nestedcss]))
         .on('error', function (errorInfo) {
             console.log(errorInfo.toString());
             this.emit('end');
