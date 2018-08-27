1d59c2e4c9f2e178c6df4c5359948cb067900d14
Michal Siron Mac <michal.siron@centrum.cz>
2018-08-27 20:42:18 +0200

    konec 8/26

--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -16,7 +16,7 @@ gulp.task('watch', function () {
     });
 
     watch("./app/assets/styles/**/*.css", function () {
-        gulp.start('cssInject');§
+        gulp.start('cssInject');
     });
 
 });
